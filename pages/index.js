0784fe41d4be5c1ce1f27e2a8d5909c69d4b6b4b
Xxanbler <xanblerschel98@gmail.com>
2025-04-18 11:33:14 -0700

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,25 @@
 function Home() {
-  return(
-  <div>
-  <h1>Teste 123</h1>;
-  <h2>segunda linha</h2>
-  <Teste />
-  </div>
+  return (
+    <div>
+      <h1>Teste 123</h1>;<h2>segunda linha</h2>
+      <Teste />
+    </div>
   );
 }
 
-function Teste(){
-  return(
+function Teste() {
+  return (
     <div>
       <h1>bora testar isso aqui!</h1>
     </div>
   );
 }
 
+function testeCommit() {
+  return (
+    <div>
+      <title>Fazendo um teste de testeCommit</title>
+    </div>
+  );
+}
 export default Home;
